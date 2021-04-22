8329f5347755d4f36d29264ac873e00f6cf3f734
Fanuel Alem <fanuelnalem@outlook.com>
2021-04-21 20:11:38 -0700

    edit

--- index.js
+++ index.js
@@ -12,13 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(fileUpload())
-const corsOptions = {
-  origin:'https://mernapp20210.herokuapp.com/',
-  optionsSuccessStatus:200
-}
-app.use(cors(corsOptions))
-app.options('*',cors())
  
+app.use(cors())
+  
  
  if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
